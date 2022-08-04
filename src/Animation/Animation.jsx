74a0dc267c171daeef512da23f8f00a2949fbef7
Mahmudjon Asqarov <mahmidjonasqarov101@gmail.com>
2022-08-04 18:27:23 +0500

    first commit

---
+++ src/Animation/Animation.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Animation.css'
+
+function Animation() {
+    return (
+        <div className="container">
+            <div className='wrapper-text' >
+                <h1>Hello world!!!</h1>
+                <h3>Hello World!!</h3>
+                <h6>Hello World!</h6>
+            </div>
+            <div className="left-top-box"></div>
+            <div className="top-right-box"></div>
+            <div className="right-bottom-box"></div>
+            <div className="left-bottom-box"></div>
+        </div>
+
+    )
+}
+
+export default Animation
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { React } from 'react';
+import Animation from './Animation/Animation';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <Animation />
+    </div>
+  );
+}
+
+export default App;
